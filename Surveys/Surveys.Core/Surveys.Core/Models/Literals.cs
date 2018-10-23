a55aa1000545cf737ecae05a9108747d1759f672
JOSE ALFREDO PONCIANO CARRANZA <mectoys2013@gmail.com>
2018-10-23 16:33:18 -0500

    1 App Encuesta
    Creacion de Listado Encuensta y Registro de Encuesta

---
+++ Surveys/Surveys.Core/Surveys.Core/Models/Literals.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Surveys.Core.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Literals
+    {
+        public const string FavoriteTeamTitle =
+            "Selección de Equipo";
+
+    }
+}
